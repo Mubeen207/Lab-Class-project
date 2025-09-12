1a9d4993cc82e2804de56b54f97fc01c6e2a50ed
irtizacoder <irtizaamjadamjadmirza@gmail.com>
2025-09-12 10:32:17 +0500

    Added Functionality

---
+++ Class Lab 7/app.js
@@ -0,0 +1,13 @@
+let sliderImages = document.getElementsByClassName("Nature");
+let topImageIndex = 0;
+
+setInterval(function () {
+  for (let i = 0; i < sliderImages.length; i++) {
+    sliderImages[i].style.zIndex = 0;
+  }
+  sliderImages[topImageIndex].style.zIndex = 10;
+  topImageIndex++;
+  if (topImageIndex >= sliderImages.length) {
+    topImageIndex = 0;
+  }
+}, 1000);
