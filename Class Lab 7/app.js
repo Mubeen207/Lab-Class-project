0f121b7f351561b4df3f0e5801d35936444b702b
irtizacoder <irtizaamjadamjadmirza@gmail.com>
2025-09-12 10:57:08 +0500

    Back and Forword Functionality Added

--- Class Lab 7/app.js
+++ Class Lab 7/app.js
@@ -11,3 +11,23 @@ setInterval(function () {
     topImageIndex = 0;
   }
 }, 3000);
+function swapRight() {
+  for (let i = 0; i < sliderImages.length; i++) {
+    sliderImages[i].style.zIndex = 0;
+  }
+  sliderImages[topImageIndex].style.zIndex = 10;
+  topImageIndex++;
+  if (topImageIndex >= sliderImages.length) {
+    topImageIndex = 0;
+  }
+}
+function swapLeft() {
+  for (let i = 0; i < sliderImages.length; i++) {
+    sliderImages[i].style.zIndex = 0;
+  }
+  topImageIndex--;
+  if (topImageIndex === 0) {
+    topImageIndex = sliderImages.length - 1;
+  }
+  sliderImages[topImageIndex].style.zIndex = 10;
+}
