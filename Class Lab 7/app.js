e0f2561dd535ec73c911ffe0a7aea39880f8848b
irtizacoder <irtizaamjadamjadmirza@gmail.com>
2025-09-12 11:03:28 +0500

    One Error

--- Class Lab 7/app.js
+++ Class Lab 7/app.js
@@ -26,8 +26,20 @@ function swapLeft() {
     sliderImages[i].style.zIndex = 0;
   }
   topImageIndex--;
-  if (topImageIndex === 0) {
+  if (topImageIndex < 0) {
     topImageIndex = sliderImages.length - 1;
   }
   sliderImages[topImageIndex].style.zIndex = 10;
 }
+// function swapLeft() {
+//     // Reset z-index for all images
+//     for (let i = 0; i < sliderImages.length; i++) {
+//         sliderImages[i].style.zIndex = 0;
+//     }
+
+//     // Decrement the index, and use the modulo operator to handle wrapping around
+//     topImageIndex = (topImageIndex - 1 + sliderImages.length) % sliderImages.length;
+    
+//     // Set the new top image
+//     sliderImages[topImageIndex].style.zIndex = 10;
+// }
