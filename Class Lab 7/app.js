173f17fb2ddafbf9f9cc2c2d11e0f597a6f6147d
mubeen207 <mubeenulislam207@gmail.com>
2025-09-14 01:59:08 +0500

    problem solved

--- Class Lab 7/app.js
+++ Class Lab 7/app.js
@@ -1,45 +1,90 @@
+// let sliderImages = document.getElementsByClassName("Nature");
+// let topImageIndex = 0;
+
+// setInterval(function () {
+//   for (let i = 0; i < sliderImages.length; i++) {
+//     sliderImages[i].style.zIndex = 0;
+//   }
+//   sliderImages[topImageIndex].style.zIndex = 10;
+//   topImageIndex++;
+//   if (topImageIndex >= sliderImages.length) {
+//     topImageIndex = 0;
+//   }
+// }, 3000);
+// function swapRight() {
+//   for (let i = 0; i < sliderImages.length; i++) {
+//     sliderImages[i].style.zIndex = 0;
+//   }
+//   sliderImages[topImageIndex].style.zIndex = 10;
+//   topImageIndex++;
+//   if (topImageIndex >= sliderImages.length) {
+//     topImageIndex = 0;
+//   }
+// }
+// function swapLeft() {
+//   for (let i = 0; i < sliderImages.length; i++) {
+//     sliderImages[i].style.zIndex = 0;
+//   }
+//   topImageIndex--;
+//   if (topImageIndex < 0) {
+//     topImageIndex = sliderImages.length - 1;
+//   }
+//   sliderImages[topImageIndex].style.zIndex = 10;
+// }
 let sliderImages = document.getElementsByClassName("Nature");
 let topImageIndex = 0;
+let intervalTime = 3000;
+let sliderInterval;
 
-setInterval(function () {
+function resetImages() {
   for (let i = 0; i < sliderImages.length; i++) {
     sliderImages[i].style.zIndex = 0;
-  }
-  sliderImages[topImageIndex].style.zIndex = 10;
-  topImageIndex++;
-  if (topImageIndex >= sliderImages.length) {
-    topImageIndex = 0;
-  }
-}, 3000);
-function swapRight() {
-  for (let i = 0; i < sliderImages.length; i++) {
-    sliderImages[i].style.zIndex = 0;
-  }
-  sliderImages[topImageIndex].style.zIndex = 10;
-  topImageIndex++;
-  if (topImageIndex >= sliderImages.length) {
-    topImageIndex = 0;
+    sliderImages[i].style.opacity = 0;
   }
 }
-function swapLeft() {
-  for (let i = 0; i < sliderImages.length; i++) {
-    sliderImages[i].style.zIndex = 0;
+
+function showImage(index) {
+  resetImages();
+  sliderImages[index].style.zIndex = 10;
+  sliderImages[index].style.opacity = 1;
+}
+
+function nextImage() {
+  topImageIndex++;
+  if (topImageIndex >= sliderImages.length) {
+    topImageIndex = 0;
   }
+  showImage(topImageIndex);
+}
+
+function prevImage() {
   topImageIndex--;
   if (topImageIndex < 0) {
     topImageIndex = sliderImages.length - 1;
   }
-  sliderImages[topImageIndex].style.zIndex = 10;
+  showImage(topImageIndex);
 }
-// function swapLeft() {
-//     // Reset z-index for all images
-//     for (let i = 0; i < sliderImages.length; i++) {
-//         sliderImages[i].style.zIndex = 0;
-//     }
 
-//     // Decrement the index, and use the modulo operator to handle wrapping around
-//     topImageIndex = (topImageIndex - 1 + sliderImages.length) % sliderImages.length;
-    
-//     // Set the new top image
-//     sliderImages[topImageIndex].style.zIndex = 10;
-// }
+function startSlider() {
+  showImage(topImageIndex);
+  sliderInterval = setInterval(nextImage, intervalTime);
+}
+
+function stopSlider() {
+  clearInterval(sliderInterval);
+}
+
+// Manual controls
+function swapRight() {
+  stopSlider();
+  nextImage();
+  startSlider();
+}
+
+function swapLeft() {
+  stopSlider();
+  prevImage();
+  startSlider();
+}
+
+startSlider();
