94946eb611d2bcb96e2e594a7ed48549bdb83db1
abdul rahman <arjanblaoch4@gmail.com>
2025-09-19 09:56:47 +0500

    callbackfunction added

--- Class Lab 8/app.js
+++ Class Lab 8/app.js
@@ -1,8 +1,27 @@
 
-console.log("1");
+// console.log("1");
 
-let MyTimeOut = setTimeout(function () {
-    console.log("3");
-}, 1000);
+// let MyTimeOut = setTimeout(function () {
+//     console.log("3");
+// }, 1000);
 
+// console.log("2");
+// console.log(MyTimeOut);
+
+console.log("3");
+
+function signup(email, pass, callback) {
+    setTimeout(function () {
+        if (email === "mubeenulislam207@gmail.com" && pass === "12345678") {
+            callback();
+        }
+    },)
+}
 console.log("2");
+
+signup("mubeenulislam207@gmail.com", "12345678", redirect);
+function redirect() {
+    console.log("Resdirect To chrome");
+
+}
+console.log("1");
