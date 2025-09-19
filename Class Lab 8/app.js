e9ded1317521dbf13530e4959ed7f26f430b9fd4
abdul rahman <arjanblaoch4@gmail.com>
2025-09-19 10:12:10 +0500

    callback seconde methode

--- Class Lab 8/app.js
+++ Class Lab 8/app.js
@@ -8,20 +8,39 @@
 // console.log("2");
 // console.log(MyTimeOut);
 
+// console.log("3");
+
+// function signup(email, pass, callback) {
+//     setTimeout(function () {
+//         if (email === "mubeenulislam207@gmail.com" && pass === "12345678") {
+//             callback();
+//         }
+//     },)
+// }
+// console.log("2");
+
+// signup("mubeenulislam207@gmail.com", "12345678", redirect);
+// function redirect() {
+//     console.log("Resdirect To chrome");
+
+// }
+// console.log("1");
 console.log("3");
 
-function signup(email, pass, callback) {
+function signup(email, pass, callback, secondecallback) {
     setTimeout(function () {
         if (email === "mubeenulislam207@gmail.com" && pass === "12345678") {
-            callback();
+            callback(secondecallback);
         }
-    },)
+    }, 1000)
 }
 console.log("2");
 
-signup("mubeenulislam207@gmail.com", "12345678", redirect);
-function redirect() {
-    console.log("Resdirect To chrome");
+signup("mubeenulislam207@gmail.com", "12345678", function redirect(log) {
+    console.log(log());
 
-}
+}, function secondefunction() {
+
+    return "Resdirect To chrome";
+});
 console.log("1");
