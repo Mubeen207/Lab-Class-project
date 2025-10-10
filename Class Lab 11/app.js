15b0827bf1978eaf1e9400f2f7ef47b0ed15b4c9
mubeen207 <mubeenulislam207@gmail.com>
2025-10-10 10:20:50 +0500

    updates

--- Class Lab 11/app.js
+++ Class Lab 11/app.js
@@ -3,7 +3,16 @@
 
 
 
-let arr = [Object, undefined, NaN, Number, String, null];
-for (let i = 0; i <= arr.length; i++) {
-    console.log(typeof arr[i], arr[i]);
+// let arr = [Object, undefined, NaN, Number, String, null];
+// for (let i = 0; i <= arr.length; i++) {
+//     console.log(typeof arr[i], arr[i]);
+// for (let i = 0; i <= arr.length; i++) {
+//     console.log(typeof arr[i], arr[i]);
+// }
+
+let string = "system";
+let result = "";
+for (let i = string.length - 1; i >= 0; i--) {
+    result = result + string[i];
 }
+console.log(result);
