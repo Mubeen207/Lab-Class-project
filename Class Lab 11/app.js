ca0aa90e0b177af81cb5b895e9e7f2cd881c261e
mubeen207 <mubeenulislam207@gmail.com>
2025-10-10 10:23:31 +0500

    error solved

--- Class Lab 11/app.js
+++ Class Lab 11/app.js
@@ -1,18 +1,17 @@
+function arrChk() {
 
-
-
-
-
-// let arr = [Object, undefined, NaN, Number, String, null];
-// for (let i = 0; i <= arr.length; i++) {
-//     console.log(typeof arr[i], arr[i]);
-// for (let i = 0; i <= arr.length; i++) {
-//     console.log(typeof arr[i], arr[i]);
-// }
-
-let string = "system";
-let result = "";
-for (let i = string.length - 1; i >= 0; i--) {
-    result = result + string[i];
+    let arr = [Object, undefined, NaN, Number, String, null];
+    for (let i = 0; i <= arr.length; i++) {
+        console.log(typeof arr[i], arr[i]);
+    }
 }
-console.log(result);
+// arrChk();
+function stringChk() {
+    let string = "system";
+    let result = '"';
+    for (let i = string.length - 1; i >= 0; i--) {
+        result = result + string[i];
+    }
+    console.log(result + '"');
+}
+stringChk();
