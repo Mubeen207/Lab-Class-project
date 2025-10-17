0b7f289f22458c6ccce950e8281569bf1bd8cd5e
ovaiskhan017873 <okahw.pk@gmail.com>
2025-10-17 10:58:10 +0500

    class 12

---
+++ Class Lab 12/app.js
@@ -0,0 +1,36 @@
+// let foodEl = document.getElementById("food");
+// let qtyEl = document.getElementById("qty");
+// let billEl = document.getElementById("bill");
+// function placeOrder() {
+//   if (qtyEl.value > 10) {
+//     alert("Okat Me Reh Lay");
+//   } else if (qtyEl.value !== "" || qtyEl.value > 0) {
+//     if (foodEl.value === "pizza") {
+//       billEl.innerHTML =
+//         qtyEl.value + " Pizza Bill Is Rs " + qtyEl.value * 1500;
+//       qtyEl.value = "";
+//     } else if (foodEl.value === "burger") {
+//       billEl.innerHTML =
+//         qtyEl.value + " Burger Bill Is Rs " + qtyEl.value * 500;
+//       qtyEl.value = "";
+//     } else if (foodEl.value === "broast") {
+//       billEl.innerHTML =
+//         qtyEl.value + " Broast Bill Is Rs " + qtyEl.value * 700;
+//       qtyEl.value = "";
+//     }
+//   } else {
+//     billEl.innerHTML = "Enter Quantity Please";
+//     qtyEl.value = "";
+//   }
+// }
+let quoteEl = document.getElementById("quote");
+let timeEl = document.getElementById("time");
+let quotesArr = [
+  "Believe you can and you're halfway there",
+  "The only way to do great work is to love what you do",
+  "If you can dream it, you can do it",
+  "Start where you are. Use what you have. Do what you can"
+];
+function randomQuote() {
+    
+}
