f736bd112fa3148c4e980a1ca287ba14833ec97f
mubeen207 <mubeenulislam207@gmail.com>
2025-10-31 10:53:31 +0500

    Class 14

---
+++ Class Lab 14/app.js
@@ -0,0 +1,20 @@
+// let firstName = "Mubeen";
+// let para = `My name is ${firstName}`
+// console.log(para);
+
+// function spread(a, ...b) {
+//   console.log(a, b);
+// }
+
+// spread("This is Spread", "Good", "ES6", "Js");
+
+// function rest(firstName, lastName) {
+//   console.log(`My name is ${firstName} ${lastName}`);
+// }
+
+// rest(...["Mubeen", "Ul islam"]);
+
+let greet = (a, b) => {
+  return a + b;
+};
+console.log(greet("Talal", " Ahmed"));
