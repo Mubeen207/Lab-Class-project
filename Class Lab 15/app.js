730b5862ee01b7fa21d442687b64da3334362421
mubeen207 <mubeenulislam207@gmail.com>
2025-11-07 09:35:05 +0500

    class 15

---
+++ Class Lab 15/app.js
@@ -0,0 +1,26 @@
+// let arr = ["Apple", "Banana", "Kiwi"];
+
+// for (let names of arr) {
+//     console.log(names);
+// }
+// for (let names in arr) {
+//     console.log(names);
+// }
+// let total = 0;
+// let arr = [10, 243, 324, 234, 54, 34, 633];
+
+// for (let numbers of arr) {
+//     total += numbers;
+// }
+// console.log(total);
+
+// for (let numbersIndex in arr) {
+//     total += arr[Number(numbersIndex)];
+// }
+// console.log(total);
+
+// let str = "Mubeen".includes("Ul Islam");
+
+// console.log(str);
+
+let 
