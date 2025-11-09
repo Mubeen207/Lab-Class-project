6a7405e012048874226d128e8deba754645f200e
mubeen207 <mubeenulislam207@gmail.com>
2025-11-09 12:47:49 +0500

    updates

--- Class Lab 3/app.js
+++ Class Lab 3/app.js
@@ -18,3 +18,16 @@ para[i].style.backgroundColor = "black"
 para[i].style.color = "white"
 para[i].innerHTML = UserInput;
 }
+
+const { use } = require("react");
+
+let divel = document.getElementsByTagName("div");
+let UserInput;
+let para = divel.getElementsByTagName("p");
+
+for (let i = 0; i < para.length; i++) {
+UserInput = prompt("Enter Your Prompt");
+para[i].style.backgroundColor = "black"
+para[i].style.color = "white"
+para[i].innerHTML = UserInput;
+}
