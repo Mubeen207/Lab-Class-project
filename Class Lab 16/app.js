0ca487a2679bad41ae80fec5f077c65b8370bc2d
Awaisishaq123 <207>
2025-11-14 10:40:55 +0500

    updates

---
+++ Class Lab 16/app.js
@@ -0,0 +1,20 @@
+// let sales = [1200, 200, 2000, 5000, 2300, 100];
+// let bigFound = 0
+// for (let getData in sales) {
+//         if (sales[getData] > bigFound) {
+//             bigFound = sales[getData]
+//         }
+// }
+// console.log(bigFound);
+//--------------------------------------------------------
+
+// let rewiews = [5, 3, 2, 5, 4];
+// let sum = 0;
+// let result = 0;
+// for (let getOne of rewiews) {
+//     sum += getOne;
+// }
+// result = sum / rewiews.length;
+// console.log(result);
+//--------------------------------------------------------
+
