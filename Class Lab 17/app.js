90df5cc433c555f35f0043e53365df49dcd78394
mubeen207 <mubeenulislam207@gmail.com>
2025-11-21 12:58:31 +0500

    bank

--- Class Lab 17/app.js
+++ Class Lab 17/app.js
@@ -1,38 +1,61 @@
-class User {
-  constructor(userName, password, email) {
-    this.userName = userName;
-    this.email = email;
-    this.password = password;
+function firstTask() {
+  class User {
+    constructor(userName, password, email) {
+      this.userName = userName;
+      this.email = email;
+      this.password = password;
+    }
+    isAdmin = false;
   }
-  isAdmin = false;
-}
 
-// class employee extends User {
-//   print() {
-//     console.log("Child2");
-//   }
-// }
-// class manager extends User {
-//   print() {
-//     console.log("Child1");
-//   }
-// }
+  // class employee extends User {
+  //   print() {
+  //     console.log("Child2");
+  //   }
+  // }
+  // class manager extends User {
+  //   print() {
+  //     console.log("Child1");
+  //   }
+  // }
 
-class Admin extends User {
-  constructor (userName, password, email , adminPrivilage) {
-
-    super(userName, password, email) 
+  class Admin extends User {
+    constructor(userName, password, email, adminPrivilage) {
+      super(userName, password, email);
       this.adminPrivilage = adminPrivilage;
+    }
+    adminDashboard = true;
   }
-  adminDashboard= true;
+
+  // let obj1 = new User("mubeen207", 123456, "mubeen@gmail.com");
+  // let childNo1 = new employee("mubeen207", 123456, "mubeen@gmail.com");
+  // let childNo2 = new manager("mubeen207", 123456, "mubeen@gmail.com");
+  let isAdmin = new Admin("mubeen207", 123456, "mubeen@gmail.com", true);
+
+  // console.log(obj1);
+  // childNo1.print();
+  // childNo2.print();
+  console.log(isAdmin);
 }
+// firstTask();
+//----------------------------------------------------------------------------------------
+function bank() {
+  class Bank {
+    constructor(balance, secretKey, withdraw, deposit) {
+      this.balance = balance;
+      this.secretKey = secretKey;
+      this.withdraw = function () {
+        console.log(withdraw);
+      };
+      this.deposit = function () {
+        console.log(deposit);
+      };
+    }
+  }
+  let bank = new Bank(1000, 1234, "withdraw", "deposit");
 
-// let obj1 = new User("mubeen207", 123456, "mubeen@gmail.com");
-// let childNo1 = new employee("mubeen207", 123456, "mubeen@gmail.com");
-// let childNo2 = new manager("mubeen207", 123456, "mubeen@gmail.com");
-let isAdmin = new Admin("mubeen207", 123456, "mubeen@gmail.com" , true);
-
-// console.log(obj1);
-// childNo1.print();
-// childNo2.print();
-console.log(isAdmin);
+  console.log(bank);
+  bank.deposit();
+  bank.withdraw();
+}
+bank();
