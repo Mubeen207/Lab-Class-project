80ed0159480213987a9c25ce561ede5b79c128a1
mubeen207 <mubeenulislam207@gmail.com>
2026-04-06 16:37:48 +0500

    gitgnpre

---
+++ class-lab-31-node-js/schema/index.js
@@ -0,0 +1,11 @@
+// import Joi from "joi";
+
+// const productSchema = Joi.object({
+//     title: Joi.string().required(),
+//     description: Joi.string().required(),
+//     price: Joi.number().integer().min(3).max(5).required(),
+// });
+
+// export {
+//     productSchema
+// }
